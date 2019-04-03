aac7fab8ca041691f4ab4c7b258450467406dfd0
baharevmv <bakharev.m.vi@sberbank.ru>
2019-04-03 23:21:46 +0300

    Creating the Project

---
+++ ConcentrationGame/ConcentrationGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcentrationGame
+//
+//  Created by Maksim Bakharev on 03/04/2019.
+//  Copyright © 2019 Maksim Bakharev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+
+
+}
+
